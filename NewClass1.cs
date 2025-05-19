2d0c776cceeeabe04bada60d3442ada3d8179c8d
Владимир Скворцов <skvortsov.vyu@edu.spbstu.ru>
2025-05-19 18:23:34 +0300

    Добавлен новый класс

---
+++ NewClass1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForm
+{
+    public class NewClass1
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+            private void SetName(string name)
+        {
+            Name = name;
+        }
+    }
+}
